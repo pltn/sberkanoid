b9944057e60e2128f81a45179c77aab707944b86
Platon Osenenko <platon.xxl@gmail.com>
2025-05-31 14:47:56 +0300

    Модульная структура

--- sberkanoid.js
+++ sberkanoid.js
@@ -1,3 +1,41 @@
 /**
  * Sberkanoid
  */
+
+const Sberkanoid = document.getElementById('sberkanoid')
+const context = Sberkanoid.getContext("2d")
+
+import { lvl_01 } from './modules/levels/level_01.js'
+import { Stick } from './modules/stick.js'
+
+class GameBoard{
+
+}
+
+class Ball {
+    constructor(r){
+        this.r = r
+    }
+
+    draw(){
+
+    }
+
+    update(){
+
+    }
+}
+
+class Brick {
+
+}
+
+class Info {
+
+}
+
+class Questions {
+
+}
+
+addEventListener
