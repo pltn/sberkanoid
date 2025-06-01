5c7e8c206e1042baa201645a920a363379594502
Platon Osenenko <platon.xxl@gmail.com>
2025-06-01 12:12:16 +0300

    Палка движется

--- modules/stick.js
+++ modules/stick.js
@@ -1,7 +1,23 @@
-class Stick {
-    constructor(width){
-        
+export default class Stick {
+    constructor(x, y, width, c){
+        console.log("created")
+        this.x = x;
+        this.y = y;
+        this.w = width;
+        this.h = 4;
+        this.features = {sticky: false}
+        this.canvasContext = c
+    }
+    draw(){
+        // console.log("drawing")
+        this.canvasContext.beginPath();
+        console.log("stick:", this.x-this.w/2, " w: ", this.w)
+        this.canvasContext.rect(this.x-this.w/2, this.y, this.w, this.h);
+        this.canvasContext.fillStyle = "#f00";
+        this.canvasContext.fill();
+        this.canvasContext.closePath()
+    }
+    updatePostion(x){
+        this.x = x
     }
 }
-
-export {Stick}
--- sberkanoid.js
+++ sberkanoid.js
@@ -3,39 +3,63 @@
  */
 
 const Sberkanoid = document.getElementById('sberkanoid')
-const context = Sberkanoid.getContext("2d")
+const ctx = Sberkanoid.getContext("2d")
+Sberkanoid.width = innerWidth;
+Sberkanoid.height = innerHeight;
 
-import { lvl_01 } from './modules/levels/level_01.js'
-import { Stick } from './modules/stick.js'
 
-class GameBoard{
+import lvl_01  from './modules/levels/level_01.js'
+import Stick from './modules/stick.js'
 
+console.log("before creation")
+const s = new Stick(Sberkanoid.width/2, Sberkanoid.height-10, 60, ctx);
+s.draw()
+
+// ctx.beginPath();
+// ctx.rect(10, 10, 50, 2);
+// ctx.fillStyle = "red";
+// ctx.fill();
+
+// class GameBoard{
+
+// }
+
+// class Ball {
+//     constructor(r){
+//         this.r = r
+//     }
+
+//     draw(){
+
+//     }
+
+//     update(){
+
+//     }
+// }
+
+// class Brick {
+
+// }
+
+// class Info {
+
+// }
+
+// class Questions {
+
+// }
+
+function animate(){
+    // console.log("go")
+    requestAnimationFrame(animate)
+    ctx.clearRect(0,0,Sberkanoid.width, Sberkanoid.height)
+    s.draw();
 }
+// По движению мыши перерисовываем положение ракетки
+Sberkanoid.addEventListener('mousemove', (e) => {
+    console.log("mouse: ", e.clientX)
+    s.updatePostion(e.clientX)
+})
 
-class Ball {
-    constructor(r){
-        this.r = r
-    }
-
-    draw(){
-
-    }
-
-    update(){
-
-    }
-}
-
-class Brick {
-
-}
-
-class Info {
-
-}
-
-class Questions {
-
-}
-
-addEventListener
+animate()
