3e8a897251a97f0b005b28f7651ed9c16b933a17
Platon Osenenko <platon.xxl@gmail.com>
2025-06-02 01:04:52 +0300

    — Событие столкновения — Уборка мусоа

--- sberkanoid.js
+++ sberkanoid.js
@@ -11,35 +11,23 @@ Sberkanoid.height = innerHeight;
 import lvl_01  from './modules/levels/level_01.js'
 import Stick from './modules/stick.js'
 import Ball from './modules/ball.js'
+import GameBoard from './modules/gameboard.js';
 
 // console.log("before creation")
+
+// Событие столкновения
+// side может принимать значения: top | bottom | left | right
+const Collision = new CustomEvent('collision', {
+    message: "Столкновение",
+    side: "top"
+})
+const g = new GameBoard(ctx)
 const s = new Stick(Sberkanoid.width/2, Sberkanoid.height-20, 100, ctx);
 const b = new Ball(s.x, s.y-20, 20, s, ctx);
-// b.draw();
-// s.draw()
 
-// ctx.beginPath();
-// ctx.rect(10, 10, 50, 2);
-// ctx.fillStyle = "red";
-// ctx.fill();
 
-// class GameBoard{
 
-// }
 
-// class Ball {
-//     constructor(r){
-//         this.r = r
-//     }
-
-//     draw(){
-
-//     }
-
-//     update(){
-
-//     }
-// }
 
 // class Brick {
 
@@ -57,12 +45,13 @@ function animate(){
     // console.log("go")
     requestAnimationFrame(animate)
     ctx.clearRect(0,0,Sberkanoid.width, Sberkanoid.height)
+    g.draw();
     s.draw();
     b.draw();
 }
 // По движению мыши перерисовываем положение ракетки
 Sberkanoid.addEventListener('mousemove', (e) => {
-    // console.log("mouse: ", e.clientX)
+    console.log("mouse: ", e.clientX)
     s.updatePostion(e.clientX)
 })
 Sberkanoid.addEventListener('click', (e) => {
