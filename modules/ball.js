fa519147a3eeb742c132686cc2098272f311d673
Platon Osenenko <platon.xxl@gmail.com>
2025-06-01 14:20:19 +0300

    Рандомный угол выстрела

--- modules/ball.js
+++ modules/ball.js
@@ -3,7 +3,16 @@ export default class Ball {
         this.onAir = false;
         this.x = x;
         this.y = y;
+        // Радиус шара
         this.r = r;
+        // Начальный угол вылета шана
+        this.getStartAngle();
+        this.a = 1.57;
+        this.v = {
+            x: 0, 
+            y: 0
+        }
+        // Палка
         this.stick = s;
         this.canvasContext = c;
         console.log("ball created");
@@ -14,14 +23,36 @@ export default class Ball {
         if(this.onAir == false){
             this.canvasContext.arc(this.stick.x, this.stick.y-this.r, this.r, 0, Math.PI*2, false)
         } else {
-            this.canvasContext.arc(s.x, this.y, this.r, 0, Math.PI*2, false)
+            this.x += this.v.x
+            this.y += this.v.y
+            this.canvasContext.arc(this.x + this.v.x, this.y + this.v.y, this.r, 0, Math.PI*2, false)
         }
         this.canvasContext.fillStyle = "green";
         this.canvasContext.fill();
         this.canvasContext.closePath();
+        
     }
     fire(){
+        this.x = this.stick.x;
+        this.y = this.stick.y - this.r;
+        this.getStartAngle()
         this.onAir = true;
-        
+    }
+    getStartAngle(){
+        const half_pi = 1.57;
+        let a = 1;
+        while (a > 0.5){
+            a = Math.random()
+        }
+        let i = Math.random()
+        if(i > 0.5){
+            this.a = half_pi + a
+        } else {
+            this.a = half_pi - a
+        }
+        this.v = {
+            x: Math.cos(this.a)*4, 
+            y: -Math.sin(this.a)*4
+        }
     }
 }
