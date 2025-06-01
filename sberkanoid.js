4945b292ed8d72cd7b22fe3241e8cb642b96d92c
Platon Osenenko <platon.xxl@gmail.com>
2025-06-01 14:21:31 +0300

    Добавление шара

--- sberkanoid.js
+++ sberkanoid.js
@@ -10,10 +10,13 @@ Sberkanoid.height = innerHeight;
 
 import lvl_01  from './modules/levels/level_01.js'
 import Stick from './modules/stick.js'
+import Ball from './modules/ball.js'
 
-console.log("before creation")
-const s = new Stick(Sberkanoid.width/2, Sberkanoid.height-10, 60, ctx);
-s.draw()
+// console.log("before creation")
+const s = new Stick(Sberkanoid.width/2, Sberkanoid.height-20, 100, ctx);
+const b = new Ball(s.x, s.y-20, 20, s, ctx);
+// b.draw();
+// s.draw()
 
 // ctx.beginPath();
 // ctx.rect(10, 10, 50, 2);
@@ -55,11 +58,14 @@ function animate(){
     requestAnimationFrame(animate)
     ctx.clearRect(0,0,Sberkanoid.width, Sberkanoid.height)
     s.draw();
+    b.draw();
 }
 // По движению мыши перерисовываем положение ракетки
 Sberkanoid.addEventListener('mousemove', (e) => {
-    console.log("mouse: ", e.clientX)
+    // console.log("mouse: ", e.clientX)
     s.updatePostion(e.clientX)
 })
-
+Sberkanoid.addEventListener('click', (e) => {
+    b.fire();
+})
 animate()
